c66a274928c4acac80b881b1a408cd35de6a7d83
Gabriel França <89952261+gabrielbessa33@users.noreply.github.com>
2021-09-02 23:06:20 -0300

    Create multi.py

---
+++ multi.py
@@ -0,0 +1,19 @@
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+#mult = num1 * num2
+
+def cal_mult():
+    mult = num1 * num2
+    print("O resuntaldo da Multiplicação é: ", round(mult, 2))
+
+def letra():
+    if num1 or num2 == str:
+        print("Só podem ser usados valores númericos!!!")
+
+
+if num1 or num2 == str:
+    letra()
+
+else:
+  cal_mult()
